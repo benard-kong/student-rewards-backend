6a2f8105078f56f5adc3c657900a6dd731ac33e4
Benard Kong <benardkong@hotmail.com>
2020-02-05 17:50:45 -0800

    Include options for server

--- src/index.js
+++ src/index.js
@@ -14,10 +14,22 @@ const context = () => ({ models })
 
 const server = new GraphQLServer({ schema, context })
 
+const options = {
+  port: 8000,
+  endpoint: '/graphql',
+  subscriptions: '/subscriptions',
+  playground: '/__graphql-playground',
+}
+
+/*
+  NOTE: Remove { force: true } in production. force: true will clean out your database each time you start your server
+*/
 sequelize.sync({ force: true }).then(async () => {
   await models.User.create({ firstName: 'Jane', lastName: 'Doe' })
   await models.User.create({ firstName: 'John', lastName: 'Doe' })
-  server.start(() => {
-    console.log('Server is running on localhost:4000')
+  server.start(options, ({ playground, port }) => {
+    console.log(
+      `Server is running on localhost:${port}; Navigate to localhost:${port}/${playground} for the GraphQL playground`
+    )
   })
 })
