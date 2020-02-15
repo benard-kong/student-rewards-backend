091038e6b47b6f563b14bb0127ef54e4bc2f1c05
Benard Kong <benardkong@hotmail.com>
2020-02-15 12:14:53 -0800

    User can ONLY change their own password; Admin cannot change other user passwords (otherwise they can also change superuser's password)

--- src/middleware/permissions/index.js
+++ src/middleware/permissions/index.js
@@ -1,4 +1,4 @@
-import { shield, and, or } from 'graphql-shield'
+import { shield, and } from 'graphql-shield'
 import * as rules from './rules'
 
 // All these rules are optional depending on your application
@@ -10,7 +10,7 @@ export const permissions = shield(
     },
     Mutation: {
       createAdminUser: rules.isSuperUser,
-      changePassword: and(rules.isAuthenticated, or(rules.isOwner, rules.isAdmin)),
+      changePassword: and(rules.isAuthenticated, rules.isOwner),
     },
   },
   {
