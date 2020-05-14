6f88336c7baacaf245f84a90ae72badecd95b95f
Benard Kong <benardkong@hotmail.com>
2020-05-13 19:41:07 -0700

    [student-transactions] Increase token expiry time for dev mode

--- src/models/user.js
+++ src/models/user.js
@@ -1,29 +1,29 @@
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const USER_ROLES = {
-  SUPER: 'SUPER',
-  ADMIN: 'ADMIN',
-  USER: 'USER',
-}
+  SUPER: "SUPER",
+  ADMIN: "ADMIN",
+  USER: "USER",
+};
 
-const passwordMinLength = 8
+const passwordMinLength = 8;
 
 const user = (sequelize, DataTypes) => {
-  const User = sequelize.define('user', {
+  const User = sequelize.define("user", {
     // the string 'user' is the name of the table in the database
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: {
-        msg: 'Email already exists',
+        msg: "Email already exists",
       },
       validate: {
         isEmail: {
-          msg: 'Invalid Email',
+          msg: "Invalid Email",
         },
         notNull: {
-          msg: 'Email is required',
+          msg: "Email is required",
         },
       },
     },
@@ -32,7 +32,7 @@ const user = (sequelize, DataTypes) => {
       allowNull: false,
       validate: {
         notNull: {
-          msg: 'Password is required',
+          msg: "Password is required",
         },
         len: {
           args: [passwordMinLength],
@@ -54,79 +54,80 @@ const user = (sequelize, DataTypes) => {
           // Checks if token added to new array is a valid JWT token. If not, throw error and do not save it in the database.
           // **********************************************
           if (newArrayObject.length > 0) {
-            const newestToken = newArrayObject[newArrayObject.length - 1]
+            const newestToken = newArrayObject[newArrayObject.length - 1];
             try {
-              jwt.verify(newestToken, process.env.JWT_SECRET_KEY)
+              jwt.verify(newestToken, process.env.JWT_SECRET_KEY);
             } catch (e) {
-              throw new Error('Invalid token. Not adding to blacklist.')
+              throw new Error("Invalid token. Not adding to blacklist.");
             }
           }
         },
       },
       defaultValue: [],
     },
-  })
+  });
 
   // Custom methods go here (often has Model.associate to teach what models are associated, name
   // DOES NOT have to be Model.associate)
-  User.customMethod = () => {}
+  User.customMethod = () => {};
 
   User.beforeCreate(async (user, options) => {
-    user.password = await user.generatePasswordHash(user.password)
-  })
+    user.password = await user.generatePasswordHash(user.password);
+  });
 
   User.checkValidToken = (token) => {
     try {
-      jwt.verify(token, process.env.JWT_SECRET_KEY)
+      jwt.verify(token, process.env.JWT_SECRET_KEY);
     } catch (err) {
-      return false
+      return false;
     }
 
-    return true
-  }
+    return true;
+  };
 
   User.cleanTokensBlacklist = async () => {
-    const allUsers = await User.findAll()
+    const allUsers = await User.findAll();
     allUsers.forEach(async (user) => {
       const newTokensBlacklist = user.tokensBlacklist.filter((token) => {
         try {
-          jwt.verify(token, process.env.JWT_SECRET_KEY)
-          return true
+          jwt.verify(token, process.env.JWT_SECRET_KEY);
+          return true;
         } catch (e) {
-          return false
+          return false;
         }
-      })
+      });
       if (newTokensBlacklist.length < user.tokensBlacklist.length) {
-        user.tokensBlacklist = newTokensBlacklist
-        await user.save()
+        user.tokensBlacklist = newTokensBlacklist;
+        await user.save();
       }
-    })
-  }
+    });
+  };
 
-  User.prototype.changePassword = async function(newPassword) {
-    if (newPassword.length < passwordMinLength) throw new Error(`Password must be >= ${passwordMinLength} characters`)
-    this.password = await this.generatePasswordHash(newPassword)
-  }
+  User.prototype.changePassword = async function (newPassword) {
+    if (newPassword.length < passwordMinLength) throw new Error(`Password must be >= ${passwordMinLength} characters`);
+    this.password = await this.generatePasswordHash(newPassword);
+  };
 
-  User.prototype.createToken = function(expiresIn = '15m') {
-    const { id, email } = this
-    return jwt.sign({ id, email }, process.env.JWT_SECRET_KEY, { expiresIn })
-  }
+  User.prototype.createToken = function (expiresIn = "15m") {
+    const _expiresIn = process.env.DEV_MODE ? "365d" : expiresIn;
+    const { id, email } = this;
+    return jwt.sign({ id, email }, process.env.JWT_SECRET_KEY, { expiresIn: _expiresIn });
+  };
 
-  User.prototype.generatePasswordHash = function(plainTextPassword) {
-    return bcrypt.hash(plainTextPassword, 10)
-  }
+  User.prototype.generatePasswordHash = function (plainTextPassword) {
+    return bcrypt.hash(plainTextPassword, 10);
+  };
 
-  User.prototype.logout = async function(token) {
-    this.tokensBlacklist = [...this.tokensBlacklist, token]
-    await this.save()
-  }
+  User.prototype.logout = async function (token) {
+    this.tokensBlacklist = [...this.tokensBlacklist, token];
+    await this.save();
+  };
 
-  User.prototype.validatePassword = function(inputPassword) {
-    return bcrypt.compare(inputPassword, this.password)
-  }
+  User.prototype.validatePassword = function (inputPassword) {
+    return bcrypt.compare(inputPassword, this.password);
+  };
 
-  return User
-}
+  return User;
+};
 
-export default user
+export default user;
