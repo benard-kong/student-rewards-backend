2afd722155506a157b9c10ab2c1827ed1684b5a6
Benard Kong <benardkong@hotmail.com>
2020-05-14 17:49:20 -0700

    [teacher-transactions] Remove uuid dependency; Add hook for trimming first and last names; Add transaction association to user

--- src/models/student.js
+++ src/models/student.js
@@ -1,66 +1,84 @@
-import { v4 as uuid } from "uuid";
+import { Sequelize } from "sequelize";
 
 const student = (sequelize, DataTypes) => {
-  const Student = sequelize.define("student", {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-    },
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: "First name is required",
+  const Student = sequelize.define(
+    "student",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "id is required for student",
+          },
+        },
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "First name is required",
+          },
+        },
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Last name is required",
+          },
+        },
+      },
+      studentNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+          msg: "Student Number already exists",
+        },
+        validate: {
+          notNull: {
+            msg: "Student Number is required",
+          },
+        },
+      },
+      grade: {
+        type: DataTypes.ENUM({
+          values: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+        }),
+      },
+      numPoints: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          isInt: true,
+          isPositive: (value) => {
+            if (value < 0) throw new Error("Points cannot be less than 0");
+          },
         },
       },
     },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: "Last name is required",
+    {
+      hooks: {
+        beforeCreate: (user, options) => {
+          user.firstName = user.firstName.trim();
+          user.lastName = user.lastName.trim();
+        },
+        beforeUpdate: (user, options) => {
+          user.firstName = user.firstName.trim();
+          user.lastName = user.lastName.trim();
         },
       },
-    },
-    studentNumber: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: {
-        msg: "Student Number already exists",
-      },
-      validate: {
-        notNull: {
-          msg: "Student Number is required",
-        },
-      },
-    },
-    grade: {
-      type: DataTypes.ENUM({
-        values: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
-      }),
-    },
-    numPoints: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        isInt: true,
-        isPositive: (value) => {
-          if (value < 0) throw new Error("Points cannot be less than 0");
-        },
-      },
-    },
-  });
+    }
+  );
 
   Student.associate = (models) => {
     Student.hasMany(models.Transaction, { onDelete: "CASCADE" });
   };
-
-  Student.beforeCreate(async (student, options) => {
-    student.id = uuid();
-  });
 
   return Student;
 };
--- src/models/user.js
+++ src/models/user.js
@@ -10,66 +10,102 @@ export const USER_ROLES = {
 const passwordMinLength = 8;
 
 const user = (sequelize, DataTypes) => {
-  const User = sequelize.define("user", {
-    // the string 'user' is the name of the table in the database
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: {
-        msg: "Email already exists",
-      },
-      validate: {
-        isEmail: {
-          msg: "Invalid Email",
+  const User = sequelize.define(
+    "user",
+    {
+      // the string 'user' is the name of the table in the database
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+          msg: "Email already exists",
         },
-        notNull: {
-          msg: "Email is required",
-        },
-      },
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: "Password is required",
-        },
-        len: {
-          args: [passwordMinLength],
-          msg: `Password must be ${passwordMinLength} or more characters`,
+        validate: {
+          isEmail: {
+            msg: "Invalid Email",
+          },
+          notNull: {
+            msg: "Email is required",
+          },
         },
       },
-    },
-    role: {
-      type: DataTypes.ENUM({
-        values: [USER_ROLES.SUPER, USER_ROLES.ADMIN, USER_ROLES.USER],
-      }),
-      defaultValue: USER_ROLES.USER,
-    },
-    tokensBlacklist: {
-      type: DataTypes.ARRAY(DataTypes.STRING),
-      validate: {
-        isValidJwt(newArrayObject) {
-          // **********************************************
-          // Checks if token added to new array is a valid JWT token. If not, throw error and do not save it in the database.
-          // **********************************************
-          if (newArrayObject.length > 0) {
-            const newestToken = newArrayObject[newArrayObject.length - 1];
-            try {
-              jwt.verify(newestToken, process.env.JWT_SECRET_KEY);
-            } catch (e) {
-              throw new Error("Invalid token. Not adding to blacklist.");
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "First name is required",
+          },
+        },
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Last name is required",
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Password is required",
+          },
+          len: {
+            args: [passwordMinLength],
+            msg: `Password must be ${passwordMinLength} or more characters`,
+          },
+        },
+      },
+      role: {
+        type: DataTypes.ENUM({
+          values: [USER_ROLES.SUPER, USER_ROLES.ADMIN, USER_ROLES.USER],
+        }),
+        defaultValue: USER_ROLES.USER,
+      },
+      tokensBlacklist: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        validate: {
+          isValidJwt(newArrayObject) {
+            // **********************************************
+            // Checks if token added to new array is a valid JWT token. If not, throw error and do not save it in the database.
+            // **********************************************
+            if (newArrayObject.length > 0) {
+              const newestToken = newArrayObject[newArrayObject.length - 1];
+              try {
+                jwt.verify(newestToken, process.env.JWT_SECRET_KEY);
+              } catch (e) {
+                throw new Error("Invalid token. Not adding to blacklist.");
+              }
             }
-          }
+          },
+        },
+        defaultValue: [],
+      },
+    },
+    {
+      hooks: {
+        beforeCreate: (user, options) => {
+          user.firstName = user.firstName.trim();
+          user.lastName = user.lastName.trim();
+        },
+        beforeUpdate: (user, options) => {
+          user.firstName = user.firstName.trim();
+          user.lastName = user.lastName.trim();
         },
       },
-      defaultValue: [],
-    },
-  });
+    }
+  );
 
   // Custom methods go here (often has Model.associate to teach what models are associated, name
   // DOES NOT have to be Model.associate)
   User.customMethod = () => {};
+  User.associate = (models) => {
+    User.hasMany(models.Transaction, { foreignKey: "teacherId", sourceKey: "id", onDelete: "CASCADE" });
+  };
 
   User.beforeCreate(async (user, options) => {
     user.password = await user.generatePasswordHash(user.password);
