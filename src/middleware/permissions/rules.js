828562727dac34f40af45614e13a7783af476cdc
Benard Kong <benardkong@hotmail.com>
2020-06-02 19:36:43 -0700

    [find-user-by-id] New Prettier/ESLint settings

--- src/middleware/permissions/rules.js
+++ src/middleware/permissions/rules.js
@@ -1,29 +1,29 @@
-import { rule, or } from 'graphql-shield'
-import { USER_ROLES } from '../../models/user'
+import { rule, or } from "graphql-shield";
+import { USER_ROLES } from "../../models/user";
 
-export const isSuperUser = rule({ cache: 'contextual' })(async (parent, args, { me }, info) => {
-  if (me?.role === USER_ROLES.SUPER) return true
-  return false
-})
+export const isSuperUser = rule({ cache: "contextual" })(async (parent, args, { me }, info) => {
+  if (me?.role === USER_ROLES.SUPER) return true;
+  return false;
+});
 
 export const isAdmin = or(
-  rule({ cache: 'contextual' })(async (parent, args, { me }, info) => {
-    if (me?.role === USER_ROLES.ADMIN) return true
-    return false
+  rule({ cache: "contextual" })(async (parent, args, { me }, info) => {
+    if (me?.role === USER_ROLES.ADMIN) return true;
+    return false;
   }),
   isSuperUser
-)
+);
 
-export const isOwner = rule({ cache: 'strict' })(async (parent, { email }, { me }, info) => {
-  if (email === me.email) return true
-  return false
-})
+export const isOwner = rule({ cache: "strict" })(async (parent, { email }, { me }, info) => {
+  if (email === me.email) return true;
+  return false;
+});
 
 export const isAuthenticated = or(
-  rule({ cache: 'contextual' })(async (parent, args, { me }, info) => {
-    if (me) return true
-    return false
+  rule({ cache: "contextual" })(async (parent, args, { me }, info) => {
+    if (me) return true;
+    return false;
   }),
   isAdmin,
   isSuperUser
-)
+);
