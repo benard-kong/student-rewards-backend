54b355ceeb3776011f8378d8a638bc91c440e92f
Benard Kong <benardkong@hotmail.com>
2020-06-21 17:43:27 -0700

    Add superuser role when creating first user

--- src/index.js
+++ src/index.js
@@ -54,58 +54,39 @@ sequelize.sync({ force: true }).then(async () => {
         firstName: "super",
         lastName: "user",
         password: "1234567890",
+        role: USER_ROLES.SUPER,
       });
     } catch (e) {
       console.error(e);
     }
   } else if (process.env.NODE_ENV === "development") {
-    let hasError = true;
-    while (hasError) {
-      try {
-        const email = await prompt("What is your email?");
-        const lastName = await prompt("What is your last name / surname(s)?");
-        const firstName = await prompt("What is your first name(s) / given name(s)?");
-        const password = await prompt("You are the superuser, please type in a password.");
-        await models.User.create({
-          email,
-          firstName,
-          lastName,
-          password,
-        });
-        hasError = false;
-      } catch (e) {
-        console.error(`There was an error: ${e.message}`);
-      }
-    }
-    /**
-     await models.User.create({
-       email: "john@doe.com",
-       firstName: "John",
-       lastName: "Doe",
-       password: "1234567890",
-       role: USER_ROLES.SUPER,
-     });
-     await models.Student.create({ firstName: "A", lastName: "B", studentNumber: "01" });
-     const firstStudent = await models.Student.findOne({ where: { studentNumber: "01" } });
-     await models.Transaction.create(
-       { numPoints: 7, studentId: firstStudent.id, teacherId: 1, createdAt: "2020-04-15T03:15:36.512Z" },
-       { Student: models.Student }
-     );
-     await models.Transaction.create(
-       { numPoints: -5, studentId: firstStudent.id, teacherId: 1, createdAt: "2020-05-01T03:15:36.512Z" },
-       { Student: models.Student }
-     );
-     await models.Transaction.create(
-       { numPoints: -1, studentId: firstStudent.id, teacherId: 1, createdAt: "2020-05-02T03:15:36.512Z" },
-       { Student: models.Student }
-     );
-     await models.Transaction.create(
-       { numPoints: 4, studentId: firstStudent.id, teacherId: 1, createdAt: "2020-03-25T03:15:36.512Z" },
-       { Student: models.Student }
-     );
-     await models.Student.create({ firstName: "C", lastName: "D", studentNumber: "02" });
-     await models.Student.create({ firstName: "E", lastName: "F", studentNumber: "03" });
-     */
+    await models.User.create({
+      email: "superuser@example.com",
+      firstName: "John",
+      lastName: "Doe",
+      password: "1234567890",
+      role: USER_ROLES.SUPER,
+    });
+    await models.Student.create({ firstName: "A", lastName: "B", studentNumber: "01" });
+    const firstStudent = await models.Student.findOne({ where: { studentNumber: "01" } });
+    await models.Transaction.create(
+      { numPoints: 7, studentId: firstStudent.id, teacherId: 1, createdAt: "2020-04-15T03:15:36.512Z" },
+      { Student: models.Student }
+    );
+    await models.Transaction.create(
+      { numPoints: -5, studentId: firstStudent.id, teacherId: 1, createdAt: "2020-05-01T03:15:36.512Z" },
+      { Student: models.Student }
+    );
+    await models.Transaction.create(
+      { numPoints: -1, studentId: firstStudent.id, teacherId: 1, createdAt: "2020-05-02T03:15:36.512Z" },
+      { Student: models.Student }
+    );
+    await models.Transaction.create(
+      { numPoints: 4, studentId: firstStudent.id, teacherId: 1, createdAt: "2020-03-25T03:15:36.512Z" },
+      { Student: models.Student }
+    );
+    await models.Student.create({ firstName: "C", lastName: "D", studentNumber: "02" });
+    await models.Student.create({ firstName: "E", lastName: "F", studentNumber: "03" });
   }
   server.start(options, ({ playground, port }) => {
     console.log(
